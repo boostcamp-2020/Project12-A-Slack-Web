ad988a7ddb3b8ce060f64e393de8d3dd9bbab8ce
2oneweek <dlgkswn885@korea.ac.kr>
2020-11-18 16:04:07 +0900

    chore: init react-typescript

---
+++ client/webpack.config.js
@@ -0,0 +1,62 @@
+/* eslint-disable */
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV === "production";
+
+module.exports = {
+  mode: isDevelopment ? "production" : "development",
+  devtool: isDevelopment ? "hidden-source-map" : "eval",
+
+  entry: ["@babel/polyfill", "./src/index.tsx"],
+
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    alias: {
+      "@atom": path.resolve(__dirname, "src/component/atom"),
+      "@molecule": path.resolve(__dirname, "src/component/molecule"),
+      "@organism": path.resolve(__dirname, "src/component/organism"),
+      "@constant": path.resolve(__dirname, "src/constant"),
+      "@page": path.resolve(__dirname, "src/page"),
+      "@store": path.resolve(__dirname, "src/store"),
+      "@util": path.resolve(__dirname, "src/util"),
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['babel-loader', 'ts-loader'],
+        exclude: "/node_modules/",
+      },
+    ],
+  },
+
+  output: {
+    filename: "main.js",
+    path: path.join(__dirname, "dist"),
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: false,
+    hot: true,
+    port: 8000,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      path: path.resolve(__dirname, "./dist"),
+      filename: "index.html",
+    }),
+    new Dotenv(),
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ["dist"],
+    }),
+  ],
+};
