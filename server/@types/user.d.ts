12a8f344d31524c16a48447e44f406d141d9e476
2oneweek <dlgkswn885@korea.ac.kr>
2020-11-22 00:02:58 +0900

    feat: declare express.User type

---
+++ server/@types/user.d.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+// global 객체를 확장하려는 경우, ambient 또는 external module을 사용해야 하는데,
+// ambient module을 사용할 수 없으므로,
+// export {} 를 통해 external module로 만들어줌
+
+declare global {
+  namespace Express {
+    interface User {
+      email: string
+      name: string
+    }
+  }
+}
+
+export {}
