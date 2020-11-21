dc169cf87b8ffdb515c3d04d10da51e36cf5440a
2oneweek <dlgkswn885@korea.ac.kr>
2020-11-22 01:32:45 +0900

    feat: add user info to req.user after through middleware verifyUser

--- server/src/middleware/verifyUser.ts
+++ server/src/middleware/verifyUser.ts
@@ -1,14 +1,20 @@
 import { Request, Response, NextFunction } from 'express'
 import jwt from '../util/jwt'
 
+type UserInfo = {
+  id: number
+  email: string
+  name: string
+}
+
 const verifyUser = (req: Request, res: Response, next: NextFunction) => {
   const {
     headers: { authorization },
   } = req
   if (!authorization) return next()
   const token = authorization.replace('bearer ', '')
-  const decoded = jwt.checkToken(token)
-  console.log(decoded)
+  const { id, email, name } = jwt.checkToken(token) as UserInfo
+  req.user = { id, email, name }
   return next()
 }
 
