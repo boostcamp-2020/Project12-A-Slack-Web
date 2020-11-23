4f2c7984ea0919a0e4ba56e19cbfb5807ba609b4
hyex <hyexkim@gmail.com>
2020-11-24 02:14:51 +0900

    feat: add error handling example using thread

--- server/src/controller/index.ts
+++ server/src/controller/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import userController from './user'
 import workspaceController from './workspace'
+import threadController from './thread'
 import verifyUser from '../middleware/verifyUser'
 
 const router = Router()
 
 router.use('/user', userController)
 router.use('/workspace', verifyUser, workspaceController)
+router.use('/thread', threadController)
 
 export default router
