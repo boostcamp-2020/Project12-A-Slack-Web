8ee0544f327d9c3f56ad7bc155eeebd48ec29c73
hyex <hyexkim@gmail.com>
2020-11-25 02:09:11 +0900

    feat: add data validation

--- server/src/service/message.service.ts
+++ server/src/service/message.service.ts
@@ -2,7 +2,25 @@ import messageModel from '../model/message.model'
 import threadModel from '../model/thread.model'
 import { statusCode, resMessage } from '../util/constant'
 
-const createMessage = async (data: object) => {
+interface MessageType {
+  userId: number
+  threadId: number
+  content: string
+}
+
+const isValidNewData = ({ userId, threadId, content }: MessageType) => {
+  if (!userId || !threadId || !content || content === '') return false
+  if (userId < 1 || threadId < 1 || Number.isNaN(threadId)) return false
+  return true
+}
+
+const createMessage = async (data: MessageType) => {
+  console.log(data)
+  if (isValidNewData(data) === false)
+    return {
+      code: statusCode.BAD_REQUEST,
+      json: { success: true, message: resMessage.OUT_OF_VALUE },
+    }
   const userId = 1
   try {
     await messageModel.create({ ...data, userId })
