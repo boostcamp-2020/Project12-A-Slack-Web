cb08d6d3e8aebf664af7be65c4c1ed9ea63d834f
hyex <hyexkim@gmail.com>
2020-11-24 14:21:02 +0900

    feat: add global style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
+import { createGlobalStyle } from 'styled-components'
 
-const App = () => <>Init</>
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  )
+}
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0px;
+    margin: 0px;
+    height: 100vh;
+    width: 100vh;
+    box-sizing: border-box;
+  }
+`
 
 export default App
