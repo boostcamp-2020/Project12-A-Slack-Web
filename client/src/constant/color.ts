4663dd716afecd0b58dbd330cdbcf67028972bd2
2oneweek <dlgkswn885@korea.ac.kr>
2020-11-24 17:40:14 +0900

    feat: add color map

---
+++ client/src/constant/color.ts
@@ -0,0 +1,33 @@
+const color = new Map()
+
+// white
+color.set('white', '#fff')
+color.set('middleWhite', '#fafbfc')
+color.set('whiteHover', '#f6f8fa')
+
+// blue
+color.set('blue', '#0368d6')
+color.set('lightBlue', '#2b89ff')
+color.set('sky', '#f1f8ff')
+
+// grey
+color.set('darkGrey', '#808080')
+color.set('grey', '#d1d5da')
+color.set('greyHover', '#f5f8fa')
+color.set('textGrey', '#576069')
+color.set('lightGrey', '#e3e3e4')
+
+// green
+color.set('green', '#2ea44f')
+color.set('greenHover', '#22863a')
+
+// red
+color.set('red', '#cb2431')
+
+// black
+color.set('black', '#000000')
+
+// ivory
+color.set('ivory', '#f3f4f6')
+
+export default color
