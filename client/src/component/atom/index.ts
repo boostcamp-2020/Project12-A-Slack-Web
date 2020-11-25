52ae54d898531b6ec5069856bd0699aaa532bbd8
zunopark <enzoribery@naver.com>
2020-11-25 21:07:21 +0900

    refactor: split files

--- client/src/component/atom/index.ts
+++ client/src/component/atom/index.ts
@@ -1,6 +1,6 @@
 import Button from './Button'
 import Image from './Image'
 import Text from './Text'
-import Icon from './Icon'
+import Icon from './Icon/Icon'
 
 export default { Button, Image, Text, Icon }
