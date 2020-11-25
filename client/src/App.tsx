99abc96f51782d2ff0a6dd1bba5061e0bc247fec
2oneweek <dlgkswn885@korea.ac.kr>
2020-11-25 13:19:33 +0900

    feat: add use localstorage token

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,64 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Switch, Route, useHistory } from 'react-router-dom'
+import myAxios from '@util/myAxios'
 import { createGlobalStyle } from 'styled-components'
-import A from '@atom'
-import M from '@molecule'
-
-const buttonStyle = {
-  height: '10rem',
-  width: '20rem',
-  backgroundColor: 'red',
-}
+import LoginPage from '@page/User/LoginPage'
+import WorkspacePage from '@page/Workspace/WorkspacePage'
 
 const App = () => {
+  const token = localStorage.getItem('token')
+  const [isAuth, setIsAuth] = useState<boolean>(false)
+  const history = useHistory()
+
+  const [_, accessToken] = window.location.search.split('=')
+  if (accessToken) {
+    localStorage.setItem('token', accessToken)
+    window.location.href = '/'
+  }
+
+  type Response = {
+    success: boolean
+    message: string
+  }
+
+  const checkToken = async (): Promise<boolean | undefined> => {
+    try {
+      const { data } = await myAxios.get<Response>('/user/status')
+      if (data.success) {
+        setIsAuth(true)
+        return true
+      }
+      return false
+    } catch (error) {
+      console.log(error)
+      return false
+    }
+  }
+
+  useEffect(() => {
+    const checkUser = async () => {
+      if (!token) {
+        history.push('/login')
+      }
+      if (token) {
+        try {
+          await checkToken()
+          history.push('/')
+        } catch (err) {
+          history.push('/login')
+        }
+      }
+    }
+    checkUser()
+  }, [])
+
   return (
     <>
       <GlobalStyle />
-      <M.ButtonDiv buttonStyle={buttonStyle}>ddd</M.ButtonDiv>
+      <Switch>
+        <Route exact path="/" component={WorkspacePage} />
+        <Route exact path="/login" component={LoginPage} />
+      </Switch>
     </>
   )
 }
