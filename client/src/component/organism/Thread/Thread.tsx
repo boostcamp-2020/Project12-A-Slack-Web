9a37e6254a4389dfdffd7d740e387f4fb05ee8c2
hyex <hyexkim@gmail.com>
2020-11-25 23:39:39 +0900

    style: add thread style

--- client/src/component/organism/Thread/Thread.tsx
+++ client/src/component/organism/Thread/Thread.tsx
@@ -1,9 +1,74 @@
 import React from 'react'
+import styled from 'styled-components'
 import A from '@atom'
-// import styled from 'styled-components'
 
 function Thread() {
-  return <div>test</div>
+  return (
+    <div>
+      <StyledContainer>
+        <StyledImageWrapper>
+          <A.Image customStyle={imageStyle} />
+        </StyledImageWrapper>
+        <StyledTextWrapper>
+          <A.Text customStyle={nameTextStyle}>이름</A.Text>
+          <A.Text customStyle={timeTextStyle}>시간</A.Text>
+          <StyledContentWrapper>
+            <A.Text customStyle={messageTextStyle}>내용</A.Text>
+          </StyledContentWrapper>
+        </StyledTextWrapper>
+      </StyledContainer>
+    </div>
+  )
 }
+
+const imageStyle = {
+  height: '36px',
+  width: '36px',
+}
+
+const nameTextStyle = {
+  fontSize: '1.5rem',
+  fontWeight: '900',
+  margin: '0px 3px 0px 0px',
+}
+
+const timeTextStyle = {
+  fontSize: '1.2rem',
+  fontWeight: '400',
+  color: 'textGrey',
+}
+
+const messageTextStyle = {
+  width: '100%',
+  fontSize: '1.5rem',
+  fontWeight: '400',
+}
+
+const StyledContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 8px 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+`
+const StyledImageWrapper = styled.div`
+  flex-shrink: 0;
+  margin-right: 8px;
+  display: flex;
+`
+const StyledTextWrapper = styled.div`
+  flex: 1 1 0;
+  min-width: 0;
+  padding: 8px;
+  padding-left: 16px;
+  margin: -12px -8px -16px -16px;
+`
+const StyledContentWrapper = styled.div`
+  width: 100%;
+  max-width: none;
+  margin-bottom: 4px;
+`
 
 export default Thread
