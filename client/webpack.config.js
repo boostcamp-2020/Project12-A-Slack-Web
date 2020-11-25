0b09ad2f4ac06ea05440aa9e18ef610d917de45f
hyex <hyexkim@gmail.com>
2020-11-25 14:22:04 +0900

    fix: add lint

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,28 +1,28 @@
 /* eslint-disable */
 
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-const isDevelopment = process.env.NODE_ENV === "production";
+const isDevelopment = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  mode: isDevelopment ? "production" : "development",
-  devtool: isDevelopment ? "hidden-source-map" : "eval",
+  mode: isDevelopment ? 'production' : 'development',
+  devtool: isDevelopment ? 'hidden-source-map' : 'eval',
 
-  entry: ["@babel/polyfill", "./src/index.tsx"],
+  entry: ['@babel/polyfill', './src/index.tsx'],
 
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
     alias: {
-      "@atom": path.resolve(__dirname, "src/component/atom"),
-      "@molecule": path.resolve(__dirname, "src/component/molecule"),
-      "@organism": path.resolve(__dirname, "src/component/organism"),
-      "@constant": path.resolve(__dirname, "src/constant"),
-      "@page": path.resolve(__dirname, "src/page"),
-      "@store": path.resolve(__dirname, "src/store"),
-      "@util": path.resolve(__dirname, "src/util"),
+      '@atom': path.resolve(__dirname, 'src/component/atom'),
+      '@molecule': path.resolve(__dirname, 'src/component/molecule'),
+      '@organism': path.resolve(__dirname, 'src/component/organism'),
+      '@constant': path.resolve(__dirname, 'src/constant'),
+      '@page': path.resolve(__dirname, 'src/page'),
+      '@store': path.resolve(__dirname, 'src/store'),
+      '@util': path.resolve(__dirname, 'src/util'),
     },
   },
 
@@ -31,18 +31,18 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: ['babel-loader', 'ts-loader'],
-        exclude: "/node_modules/",
+        exclude: '/node_modules/',
       },
     ],
   },
 
   output: {
-    filename: "main.js",
-    path: path.join(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.join(__dirname, 'dist'),
   },
 
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     compress: false,
     hot: true,
     port: 8000,
@@ -50,13 +50,13 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      path: path.resolve(__dirname, "./dist"),
-      filename: "index.html",
+      template: './public/index.html',
+      path: path.resolve(__dirname, './dist'),
+      filename: 'index.html',
     }),
     new Dotenv(),
     new CleanWebpackPlugin({
-      cleanAfterEveryBuildPatterns: ["dist"],
+      cleanAfterEveryBuildPatterns: ['dist'],
     }),
   ],
-};
+}
