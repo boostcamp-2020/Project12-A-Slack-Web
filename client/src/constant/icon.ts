39137b2ea58b2e0fe0542692f1368c3151a5f27f
zunopark <enzoribery@naver.com>
2020-11-26 18:36:09 +0900

    feat: add close icon

--- client/src/constant/icon.ts
+++ client/src/constant/icon.ts
@@ -2,8 +2,14 @@ import {
   faClock,
   faQuestionCircle,
   faSearch,
+  faTimes,
 } from '@fortawesome/free-solid-svg-icons'
 
-const myIcon = { clock: faClock, question: faQuestionCircle, search: faSearch }
+const myIcon = {
+  clock: faClock,
+  question: faQuestionCircle,
+  search: faSearch,
+  close: faTimes,
+}
 
 export default myIcon
