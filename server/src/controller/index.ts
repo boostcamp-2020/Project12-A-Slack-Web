56ffa72838d8281887b1c7aa087daf36bd344667
2oneweek <dlgkswn885@korea.ac.kr>
2020-11-27 00:17:51 +0900

    feat: add router

--- server/src/controller/index.ts
+++ server/src/controller/index.ts
@@ -1,18 +1,22 @@
 import { Router } from 'express'
 import verifyUser from '../middleware/user.middleware'
-import userController from './user'
-import channelController from './channel'
-import workspaceController from './workspace'
-import threadController from './thread'
-import messageController from './message'
+import userRouter from './user'
+import workspaceRouter from './workspace'
+import channelRouter from './channel'
+import threadRouter from './thread'
+import messageRouter from './message'
+import reactionRouter from './reaction'
+import fileRouter from './file'
 
 const router = Router()
 
-router.use('/user', userController)
+router.use('/user', userRouter)
 router.use(verifyUser)
-router.use('/channel', channelController)
-router.use('/workspace', workspaceController)
-router.use('/thread', threadController)
-router.use('/message', messageController)
+router.use('/workspace', workspaceRouter)
+router.use('/channel', channelRouter)
+router.use('/thread', threadRouter)
+router.use('/message', messageRouter)
+router.use('/reaction', reactionRouter)
+router.use('file', fileRouter)
 
 export default router
