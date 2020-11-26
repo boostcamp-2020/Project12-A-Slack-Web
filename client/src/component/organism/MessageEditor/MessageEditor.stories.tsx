9b7dfc30b2d46a22b65d2e83ea8a9abfd3586a8d
hyex <hyexkim@gmail.com>
2020-11-27 02:43:59 +0900

    feat: add messageEditor (failure)

--- client/src/component/organism/MessageEditor/MessageEditor.stories.tsx
+++ client/src/component/organism/MessageEditor/MessageEditor.stories.tsx
@@ -7,9 +7,17 @@ export default {
 }
 
 export const messageEditor = () => {
-  return <MessageEditor id={1} />
+  return <MessageEditor placeHolder="Jot something down" />
 }
 
 messageEditor.story = {
   name: 'Default',
+}
+
+export const editMessageEditor = () => {
+  return <MessageEditor id={1} value="<p><strong>hello</strong> world </p>" />
+}
+
+export const replyMessageEditor = () => {
+  return <MessageEditor placeHolder="Reply..." />
 }
--- client/src/component/organism/MessageEditor/MessageEditor.tsx
+++ client/src/component/organism/MessageEditor/MessageEditor.tsx
@@ -1,25 +1,78 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { createRef, useEffect } from 'react'
+// import 'codemirror/lib/codemirror.css'
 import '@toast-ui/editor/dist/toastui-editor.css'
 import { Editor } from '@toast-ui/react-editor'
-import A from '@atom'
+import Styled from './MessageEditor.style'
 
-// later: userId from store
 interface MessageEditorProps {
-  id: number
+  id?: number
+  value?: string
+  placeHolder?: string
 }
 
-function MessageEditor({ id }: MessageEditorProps) {
+function MessageEditor({ id, value, placeHolder }: MessageEditorProps) {
+  const editorRef = createRef<Editor>()
+
+  const handleEditorClick = () => {
+    const editor = editorRef.current?.getInstance()
+    console.log('CREATE MESSAGE !')
+    console.log(editor?.getHtml())
+  }
+
+  const createLastButton = () => {
+    const button = document.createElement('button')
+    button.addEventListener('click', () => handleEditorClick())
+
+    return button
+  }
+
+  useEffect(() => {
+    const editor = editorRef.current?.getInstance()
+    editor?.setHtml(value, true)
+  }, [])
+
   return (
-    <>
+    <Styled.Container>
       <Editor
         previewStyle="vertical"
-        height="300px"
+        height="100px"
         initialEditType="wysiwyg"
-        placeholder="글쓰기"
+        placeholder={placeHolder}
+        ref={editorRef}
+        hideModeSwitch
+        toolbarItems={[
+          'bold',
+          'italic',
+          'strike',
+          'code',
+          'link',
+          'ul',
+          'ol',
+          'quote',
+          'codeblock',
+          'image',
+          'divider',
+          {
+            type: 'button',
+            options: {
+              el: createLastButton(),
+              className: 'last',
+              event: 'click',
+              tooltip: 'Create message',
+              text: '>',
+              style: 'color:black;position:absolute;right:0;margin-right:15px',
+            },
+          },
+        ]}
       />
-    </>
+    </Styled.Container>
   )
+}
+
+MessageEditor.defaultProps = {
+  id: 0,
+  value: '',
+  placeHolder: 'Jot something down',
 }
 
 export default MessageEditor
