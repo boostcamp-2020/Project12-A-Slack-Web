4a85e03fb5e26811cc7d8798d6d301fdb2280946
kimseoyoung <caribou503@gmail.com>
2020-11-27 01:15:21 +0900

    feat: add MessageActionsMenu organism component
    Modal + ActionMenuButton 조합으로 ActionBar의 more actions 버튼을 클릭하면 나타나는 컴포넌트 구현

---
+++ client/src/component/organism/MessageActionsMenu/index.ts
@@ -0,0 +1,10 @@
+import { ModalWrapperType } from '@atom/ModalWrapper'
+
+export { default } from './MessageActionsMenu'
+
+export interface MessageActionsMenuProps {
+  targetId: number
+  modalAttributes?: ModalWrapperType.StyleAttributes
+  onDeleteButtonClick: () => void
+  onEditButtonClick: () => void
+}
