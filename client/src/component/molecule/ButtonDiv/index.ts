73073f18a4f44e7cdd7f1eb4590f3b13884797bc
kimseoyoung <caribou503@gmail.com>
2020-11-26 17:07:46 +0900

    refactor: split ButtonDiv molecule

---
+++ client/src/component/molecule/ButtonDiv/index.ts
@@ -0,0 +1,12 @@
+import { ReactChild } from 'react'
+import { ButtonType } from '@atom/Button'
+import { TextType } from '@atom/Text'
+
+export { default } from './ButtonDiv'
+
+export interface ButtonDivProps {
+  buttonStyle?: ButtonType.StyleAttributes
+  textStyle?: TextType.StyleAttributes
+  children?: ReactChild
+  onClick?: () => void
+}
