e14ca57f59f5f4337a566e5f85b2339c1065cf3e
hyex <hyexkim@gmail.com>
2020-11-26 14:32:46 +0900

    refactor: add replybutton folder and split files

--- client/src/component/molecule/ReplyButton/ReplyButton.tsx
+++ client/src/component/molecule/ReplyButton/ReplyButton.tsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
 import A from '@atom'
-
-interface ReplyButtonProps {
-  count: number
-  time: string
-  onClick: () => void
-}
+import { ImageType } from '@atom/Image'
+import { TextType } from '@atom/Text'
+import { ReplyButtonProps } from '.'
+import Styled from './ReplyButton.style'
 
 const ReplyButton = ({ count, time, onClick }: ReplyButtonProps) => {
   const [hover, setHover] = useState(false)
@@ -17,89 +14,43 @@ const ReplyButton = ({ count, time, onClick }: ReplyButtonProps) => {
 
   return (
     <>
-      <StyledContainer
+      <Styled.Container
         onMouseOver={handleMouseOver}
         onMouseOut={handleMouseOut}
         onClick={handleClick}
       >
-        <StyledImageWrapper>
+        <Styled.ImageWrapper>
           <A.Image customStyle={imageStyle} />
-        </StyledImageWrapper>
-        <StyledCountTextWrapper>
+        </Styled.ImageWrapper>
+        <Styled.CountTextWrapper>
           <A.Text customStyle={countTextStyle}>{`${count} reply`}</A.Text>
-        </StyledCountTextWrapper>
-        <StyledTimeTextWrapper>
+        </Styled.CountTextWrapper>
+        <Styled.TimeTextWrapper>
           <A.Text customStyle={timeTextStyle}>{time}</A.Text>
-        </StyledTimeTextWrapper>
-        {hover && <StyledArrow>&gt;</StyledArrow>}
-      </StyledContainer>
+        </Styled.TimeTextWrapper>
+        {hover && <Styled.Arrow>&gt;</Styled.Arrow>}
+      </Styled.Container>
     </>
   )
 }
 
 ReplyButton.defaultProps = {}
 
-const imageStyle = {
+const imageStyle: ImageType.StyleAttributes = {
   height: '24px',
   width: '24px',
 }
 
-const countTextStyle = {
+const countTextStyle: TextType.StyleAttributes = {
   fontSize: '1.3rem',
   fontWeight: '700',
   color: 'linkBlue',
 }
 
-const timeTextStyle = {
+const timeTextStyle: TextType.StyleAttributes = {
   fontSize: '1.3rem',
   fontWeight: '400',
   color: 'textGrey',
 }
-
-const StyledContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  margin-top: -4px;
-  margin-bottom: 4px;
-  margin: 4px 0 -2px -5px;
-  display: flex;
-  align-items: center;
-  max-width: 600px;
-  padding: 4px;
-  font-size: 13px;
-  border: 1px solid transparent;
-  border-radius: 6px;
-  cursor: pointer;
-  overflow: hidden;
-  height: 34px;
-  &:hover {
-    border-color: rgba(97, 96, 97, 0.13);
-  }
-`
-const StyledImageWrapper = styled.div`
-  margin-right: 4px;
-  flex-shrink: 0;
-`
-const StyledCountTextWrapper = styled.div`
-  margin-left: 3px;
-  flex-shrink: 0;
-`
-const StyledTimeTextWrapper = styled.div`
-  margin-left: 8px;
-  flex: 1;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-`
-
-const StyledArrow = styled.div`
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 400;
-  display: inline-block;
-  color: #868686;
-  margin-left: auto;
-  margin-right: 10px;
-`
 
 export default ReplyButton
