8494d362a8eaf5d174ef042ad61df8e375c90608
2oneweek <dlgkswn885@korea.ac.kr>
2020-11-27 21:43:42 +0900

    fix: change logic of file upload api

--- server/src/controller/file/file.controller.ts
+++ server/src/controller/file/file.controller.ts
@@ -1,14 +1,11 @@
 import { Request, Response, NextFunction } from 'express'
-import fileService from '@service/file.service'
+import { statusCode } from '@util/constant'
 
 const createFile = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { code, json } = await fileService.createFile({
-      url: req.file.path,
-      type: req.file.mimetype,
-      messageId: +req.body.messageId,
-    })
-    return res.status(code).json(json)
+    return res
+      .status(statusCode.OK)
+      .json({ success: true, filePath: req.file.path })
   } catch (error) {
     return next(error)
   }
