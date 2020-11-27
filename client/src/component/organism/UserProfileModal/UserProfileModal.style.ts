c9c7c5a66a25fd7d5c6bab361dc177006b79e797
kimseoyoung <caribou503@gmail.com>
2020-11-28 07:53:32 +0900

    feat: add UserProfileModal organism

---
+++ client/src/component/organism/UserProfileModal/UserProfileModal.style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const BottomWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 17px;
+`
+
+export default {
+  Wrapper,
+  BottomWrapper,
+}
