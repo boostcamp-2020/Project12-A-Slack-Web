9349db260e555ac107a6a07b636610d8f59c16b0
2oneweek <dlgkswn885@korea.ac.kr>
2020-11-27 22:07:17 +0900

    fix: file create api return

--- server/src/controller/file/file.controller.ts
+++ server/src/controller/file/file.controller.ts
@@ -3,9 +3,11 @@ import { statusCode } from '@util/constant'
 
 const createFile = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    return res
-      .status(statusCode.OK)
-      .json({ success: true, filePath: req.file.path })
+    return res.status(statusCode.OK).json({
+      success: true,
+      filePath: req.file.path,
+      fileType: req.file.mimetype,
+    })
   } catch (error) {
     return next(error)
   }
