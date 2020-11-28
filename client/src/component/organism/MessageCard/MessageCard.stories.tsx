e37b16b07f137c07888442e8d77a39f30b586f7b
hyex <hyexkim@gmail.com>
2020-11-28 17:02:36 +0900

    feat: add knobs to storybook
    - storybook 상에서 props 변경 가능하게 해주는 knobs 라이브러리 설치, 적용

--- client/src/component/organism/MessageCard/MessageCard.stories.tsx
+++ client/src/component/organism/MessageCard/MessageCard.stories.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { withKnobs, object, boolean, number } from '@storybook/addon-knobs'
 import MessageCard from '.'
 
 export default {
   title: 'Organism/MessageCard',
   component: MessageCard,
+  decorators: [withKnobs],
 }
 
 export const messageCard = () => {
-  return <MessageCard data={{}} />
+  const continuous = boolean('continuous', false)
+  const data = object('data', { id: 0 })
+  return <MessageCard data={data} continuous={continuous} />
 }
 
 messageCard.story = {
