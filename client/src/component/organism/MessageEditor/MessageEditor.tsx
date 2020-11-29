9e0763383fcbbe03fafb372f119b9013a0226265
hyex <hyexkim@gmail.com>
2020-11-29 14:31:13 +0900

    refacror: delete toast ui

--- client/src/component/organism/MessageEditor/MessageEditor.tsx
+++ client/src/component/organism/MessageEditor/MessageEditor.tsx
@@ -1,6 +1,4 @@
-import React, { createRef, useEffect } from 'react'
-import '../../../../node_modules/@toast-ui/editor/dist/toastui-editor.css'
-import { Editor } from '@toast-ui/react-editor'
+import React from 'react'
 import Styled from './MessageEditor.style'
 
 interface MessageEditorProps {
@@ -10,12 +8,8 @@ interface MessageEditorProps {
 }
 
 function MessageEditor({ id, value, placeHolder }: MessageEditorProps) {
-  const editorRef = createRef<Editor>()
-
   const handleEditorClick = () => {
-    const editor = editorRef.current?.getInstance()
     console.log('CREATE MESSAGE !')
-    console.log(editor?.getHtml())
   }
 
   const createLastButton = () => {
@@ -25,45 +19,8 @@ function MessageEditor({ id, value, placeHolder }: MessageEditorProps) {
     return button
   }
 
-  useEffect(() => {
-    const editor = editorRef.current?.getInstance()
-    if (value) editor?.setHtml(value, true)
-  }, [])
-
   return (
     <Styled.Container>
-      <Editor
-        previewStyle="vertical"
-        height="100px"
-        initialEditType="wysiwyg"
-        placeholder={placeHolder}
-        ref={editorRef}
-        hideModeSwitch
-        toolbarItems={[
-          'bold',
-          'italic',
-          'strike',
-          'code',
-          'link',
-          'ul',
-          'ol',
-          'quote',
-          'codeblock',
-          'image',
-          'divider',
-          {
-            type: 'button',
-            options: {
-              el: createLastButton(),
-              className: 'last',
-              event: 'click',
-              tooltip: 'Create message',
-              text: '>',
-              style: 'color:black;position:absolute;right:0;margin-right:15px',
-            },
-          },
-        ]}
-      />
     </Styled.Container>
   )
 }
