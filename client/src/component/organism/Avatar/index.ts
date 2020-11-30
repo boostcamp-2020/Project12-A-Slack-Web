ba035058696e084e5098416896ca65fecfc898c1
2oneweek <dlgkswn885@korea.ac.kr>
2020-11-30 14:35:15 +0900

    fix: errors

--- client/src/component/organism/Avatar/index.ts
+++ client/src/component/organism/Avatar/index.ts
@@ -3,7 +3,12 @@ import { ImageType } from '@atom/Image'
 export { default } from './Avatar'
 
 export interface AvatarProps {
-  user: object
+  user: {
+    id: number
+    email: string
+    name: string
+    profileImageUrl: string
+  }
   size: 'SMALL' | 'MEDIUM' | 'BIG'
   clickable?: boolean
   avatarImageStyle?: ImageType.StyleAttributes
--- client/src/component/organism/UserProfileModal/index.ts
+++ client/src/component/organism/UserProfileModal/index.ts
@@ -3,7 +3,12 @@ import { ModalWrapperType } from '@atom/ModalWrapper'
 export { default } from './UserProfileModal'
 
 export interface UserProfileModalProps {
-  user: object
+  user: {
+    id: number
+    email: string
+    name: string
+    profileImageUrl: string
+  }
   modalAttributes?: ModalWrapperType.StyleAttributes
   onMessageButtonClick?: () => void
   onClose?: () => void
