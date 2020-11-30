a71cda74fd46866e1927522e172a08cf18d10bbb
kimseoyoung <caribou503@gmail.com>
2020-11-30 20:28:24 +0900

    feat: add ChannelCard organism component

---
+++ client/src/component/organism/ChannelCard/ChannelCard.tsx
@@ -0,0 +1,120 @@
+import React, { useState } from 'react'
+import A from '@atom'
+import M from '@molecule'
+import color from '@constant/color'
+import myIcon from '@constant/icon'
+import { ButtonType } from '@atom/Button'
+import { TextType } from '@atom/Text'
+import { IconType } from '@atom/Icon'
+import { ChannelCardProps } from '.'
+
+import Styled from './ChannelCard.style'
+
+const ChannelHeader = ({
+  channel,
+  onJoinButtonClick,
+  onLeaveButtonClick,
+}: ChannelCardProps) => {
+  const { id, name, type, memberCount, joined } = channel
+
+  const [hover, setHover] = useState<boolean>(false)
+
+  const handleMouseEnter = () => setHover(true)
+  const handleMouseLeave = () => setHover(false)
+
+  const handleButtonClick = joined ? onLeaveButtonClick : onJoinButtonClick
+  const optionalButtonStyle = joined ? leaveButtonStyle : joinButtonStyle
+  const optionalButtonTextStyle = joined ? {} : { color: 'white' }
+  const buttonText = joined ? 'Leave' : 'Join'
+
+  return (
+    <Styled.Wrapper
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <Styled.ChannelInfoWrapper>
+        <Styled.ChannelSubTextWrapper>
+          <A.Icon icon={type === 'PUBLIC' ? myIcon.hashtag : myIcon.lock} />
+          <A.Text customStyle={channelNameTextStyle}>{name}</A.Text>
+        </Styled.ChannelSubTextWrapper>
+
+        <Styled.ChannelSubTextWrapper>
+          {joined && (
+            <A.Text customStyle={joinedTextStyle}>
+              <>
+                <A.Icon icon={myIcon.check} customStyle={joinedIconStyle} />
+                Joined
+              </>
+            </A.Text>
+          )}
+
+          <A.Text customStyle={memberCountTextStyle}>
+            {memberCount + (memberCount > 1 ? ' members' : ' member')}
+          </A.Text>
+        </Styled.ChannelSubTextWrapper>
+      </Styled.ChannelInfoWrapper>
+
+      {hover && (
+        <M.ButtonDiv
+          onClick={handleButtonClick}
+          buttonStyle={{
+            ...buttonStyle,
+            ...optionalButtonStyle,
+          }}
+          textStyle={{ ...buttonTextStyle, ...optionalButtonTextStyle }}
+        >
+          {buttonText}
+        </M.ButtonDiv>
+      )}
+    </Styled.Wrapper>
+  )
+}
+
+ChannelHeader.defaultProps = {}
+
+const channelNameTextStyle: TextType.StyleAttributes = {
+  fontWeight: 'bold',
+  cursor: 'pointer',
+  margin: '0 0 0 5px',
+  fontSize: '1.6rem',
+}
+
+const buttonStyle: ButtonType.StyleAttributes = {
+  padding: '10px',
+  width: '80px',
+  height: '36px',
+}
+const buttonTextStyle: TextType.StyleAttributes = {
+  fontWeight: '500',
+  fontSize: '1.4rem',
+}
+
+const leaveButtonStyle: ButtonType.StyleAttributes = {
+  border: `1px solid ${color.get('darkGrey')}`,
+  backgroundColor: 'white',
+  hoverBackgroundColor: 'greyHover',
+}
+
+const joinButtonStyle: ButtonType.StyleAttributes = {
+  border: `1px solid ${color.get('green')}`,
+  backgroundColor: 'green',
+  hoverBackgroundColor: 'greenHover',
+}
+
+const joinedTextStyle: TextType.StyleAttributes = {
+  color: 'green',
+  fontSize: '1.3rem',
+  margin: '0 10px 0 0',
+}
+
+const joinedIconStyle: IconType.StyleAttributes = {
+  color: 'green',
+  margin: '0 5px 0 0',
+}
+
+const memberCountTextStyle: TextType.StyleAttributes = {
+  color: 'darkGrey',
+  fontSize: '1.3rem',
+}
+
+export default ChannelHeader
