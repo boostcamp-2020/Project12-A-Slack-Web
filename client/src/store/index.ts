89726ba7fee557572ebf25bd75cc28857c421b2e
2oneweek <dlgkswn885@korea.ac.kr>
2020-12-02 03:21:04 +0900

    chore: change file name

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,10 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { workspaceStore } from './workspace.store'
-import thread from './thread'
+import workspaceStore from './workspace.store'
+import threadStore from './thread.store'
 
-const rootReducer = combineReducers({ workspaceStore, thread })
+const rootReducer = combineReducers({ workspaceStore, threadStore })
 
 export type RootState = ReturnType<typeof rootReducer>
 
