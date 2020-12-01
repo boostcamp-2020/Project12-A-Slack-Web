81b2b29283f3c64bf837f3c4f4c4ff910c10e6e9
hyex <hyexkim@gmail.com>
2020-12-01 16:56:46 +0900

    feat: create thread test with store in message editor
    - 리덕스의 CREATE_THREAD 액션 테스트

---
+++ client/src/store/reducer/mock-data.ts
@@ -0,0 +1,60 @@
+export default {
+  threadList: [
+    {
+      id: 1,
+      createdAt: '2020-11-25T15:09:30.000Z',
+      updatedAt: '2020-11-25T15:09:30.000Z',
+      messageCount: 3,
+      profileImageUrl: ['1', '2', '3'],
+      User: {
+        id: 2,
+        email: 'ihanju95@gmail.com',
+        name: '이두주',
+        profileImageUrl:
+          'https://lh3.googleusercontent.com/-VDkRdj9PpUo/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucnTBFod0S-59xYDXy2Y5oG8kAFYnA/s96-c/photo.jpg',
+      },
+    },
+    {
+      id: 2,
+      createdAt: '2020-11-25T15:09:30.000Z',
+      updatedAt: '2020-11-25T15:09:30.000Z',
+      messageCount: 3,
+      profileImageUrl: ['1', '2', '3'],
+      User: {
+        id: 1,
+        email: 'dlgkswn885@korea.ac.kr',
+        name: '‍이한주[ 학부재학 / 산업경영공학부 ]',
+        profileImageUrl:
+          'https://lh6.googleusercontent.com/-N1Pn-Or52MM/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucn7DRZcFBqsGNFc9Z5LUf8hGZRi5g/s96-c/photo.jpg',
+      },
+    },
+    {
+      id: 3,
+      createdAt: '2020-11-26T11:12:41.000Z',
+      updatedAt: '2020-11-26T11:12:41.000Z',
+      messageCount: 0,
+      profileImageUrl: [],
+      User: {
+        id: 1,
+        email: 'dlgkswn885@korea.ac.kr',
+        name: '‍이한주[ 학부재학 / 산업경영공학부 ]',
+        profileImageUrl:
+          'https://lh6.googleusercontent.com/-N1Pn-Or52MM/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucn7DRZcFBqsGNFc9Z5LUf8hGZRi5g/s96-c/photo.jpg',
+      },
+    },
+    {
+      id: 4,
+      createdAt: '2020-11-26T11:25:44.000Z',
+      updatedAt: '2020-11-26T11:25:44.000Z',
+      messageCount: 1,
+      profileImageUrl: ['1'],
+      User: {
+        id: 1,
+        email: 'dlgkswn885@korea.ac.kr',
+        name: '‍이한주[ 학부재학 / 산업경영공학부 ]',
+        profileImageUrl:
+          'https://lh6.googleusercontent.com/-N1Pn-Or52MM/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucn7DRZcFBqsGNFc9Z5LUf8hGZRi5g/s96-c/photo.jpg',
+      },
+    },
+  ],
+}
--- client/src/store/reducer/thread.ts
+++ client/src/store/reducer/thread.ts
@@ -1,15 +1,25 @@
+
+interface UserType {
+  id: number
+  email: string
+  name: string
+  profileImageUrl: string
+}
+
 export interface ThreadType {
   id: number
   createdAt: string
   updatedAt: string
   messageCount: number
-  profileImageUrl: string[]
+  profileImageUrl: string[] | null
+  User: UserType
 }
 
 // Action
 export const GET_THREADS = 'thread/GET_THREADS' as const
 export const GET_THREADS_SUCCESS = 'thread/GET_THREADS_SUCCESS' as const
 export const GET_THREADS_ERROR = 'thread/GET_THREADS_ERROR' as const
+export const CREATE_THREAD = 'thread/CREATE_THREAD' as const
 
 // Action generator
 export const getThreads = () => ({
@@ -22,12 +32,17 @@ export const getThreadsSuccess = (threads: ThreadType[]) => ({
 export const getThreadsError = () => ({
   type: GET_THREADS_ERROR,
 })
+export const createThread = (newThread: ThreadType) => ({
+  type: CREATE_THREAD,
+  payload: newThread,
+})
 
 // actions
 type ThreadAction =
   | ReturnType<typeof getThreads>
   | ReturnType<typeof getThreadsSuccess>
   | ReturnType<typeof getThreadsError>
+  | ReturnType<typeof createThread>
 
 // state
 interface ThreadState {
@@ -41,7 +56,7 @@ const initialState: ThreadState = {
 
 // reducer
 function thread(
-  state: ThreadState = initialState,
+  state: ThreadState = mockData,
   action: ThreadAction,
 ): ThreadState {
   switch (action.type) {
@@ -49,6 +64,8 @@ function thread(
     case GET_THREADS_SUCCESS:
       return { threadList: action.payload }
     // case GET_THREADS_ERROR:
+    case CREATE_THREAD:
+      return { threadList: [...state.threadList, action.payload] }
     default:
       return state
   }
