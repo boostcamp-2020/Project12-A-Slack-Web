7897a19eabd43a61d3aab464bf82a92655429183
2oneweek <dlgkswn885@korea.ac.kr>
2020-12-02 00:55:38 +0900

    feat: style login page

--- client/src/page/User/LoginPage.tsx
+++ client/src/page/User/LoginPage.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import M from '@molecule'
+import styled from 'styled-components'
 
 const LoginPage = () => {
+  const serverURL =
+    process.env.NODE_ENV === 'development'
+      ? process.env.SERVER_DOMAIN_DEVELOP
+      : process.env.SERVER_DOMAIN_PRODUCTION
+
   return (
-    <a href="http://localhost:3000/api/user/oauth/google">
-      <M.ButtonDiv>구글로그인</M.ButtonDiv>
-    </a>
+    <LoginContainer>
+      <a href={`${serverURL}/api/user/oauth/google`}>
+        <M.ButtonDiv>구글로그인</M.ButtonDiv>
+      </a>
+    </LoginContainer>
   )
 }
+
+const LoginContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
 
 export default LoginPage
