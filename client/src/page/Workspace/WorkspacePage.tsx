e1ec39ff0c79f2c81d3ec362c26d27a0e96b3e19
2oneweek <dlgkswn885@korea.ac.kr>
2020-12-02 00:55:57 +0900

    feat: render workspace list and create workspace

--- client/src/page/Workspace/WorkspacePage.tsx
+++ client/src/page/Workspace/WorkspacePage.tsx
@@ -1,8 +1,85 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import A from '@atom'
 import M from '@molecule'
+import styled from 'styled-components'
+import { RootState } from '@store/index'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  readWorkspaceLoading,
+  readWorkspaceSuccess,
+  readWorkspaceError,
+  onChangeWorkspaceInput,
+  createNewWorkspace,
+} from '@store/workspace.store'
+import myAxios from '@util/myAxios'
 
 const WorkspacePage = () => {
-  return <M.ButtonDiv>워크 스페이스 페이지</M.ButtonDiv>
+  const workspaceStore = useSelector((state: RootState) => state.workspaceStore)
+  const dispatch = useDispatch()
+
+  const getWorkspaceByUser = async (): Promise<void> => {
+    dispatch(readWorkspaceLoading())
+    try {
+      const {
+        data: { success, message, data },
+      } = await myAxios.get({ path: '/workspace' })
+      if (success) dispatch(readWorkspaceSuccess(data))
+      if (message) alert(message)
+    } catch (error) {
+      dispatch(readWorkspaceError(error))
+    }
+  }
+
+  const handleNewWorkspaceInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    console.log(name, value)
+    dispatch(onChangeWorkspaceInput(name, value))
+  }
+
+  const handleClickCreateNewWorkspace = () => {
+    dispatch(createNewWorkspace())
+  }
+
+  useEffect(() => {
+    console.log(workspaceStore)
+    getWorkspaceByUser()
+  }, [])
+
+  return (
+    <WorkspaceContainer>
+      {workspaceStore.loading ? (
+        <M.ButtonDiv>Loading...</M.ButtonDiv>
+      ) : (
+        workspaceStore.workspaces.map((workspace) => {
+          return (
+            <M.ButtonDiv buttonStyle={WorkspaceButtonStyle}>
+              {workspace.name}
+            </M.ButtonDiv>
+          )
+        })
+      )}
+      <A.Input name="name" onChange={handleNewWorkspaceInput} />
+      <M.ButtonDiv onClick={handleClickCreateNewWorkspace}>
+        생성하기
+      </M.ButtonDiv>
+    </WorkspaceContainer>
+  )
+}
+
+const WorkspaceContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+const WorkspaceButtonStyle = {
+  width: '10rem',
+  height: '2rem',
+  border: '1px solid lightGrey',
 }
 
 export default WorkspacePage
