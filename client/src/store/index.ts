ea09cd396424368bf4e1a7a5324888247793138b
hyex <hyexkim@gmail.com>
2020-12-01 19:35:51 +0900

    feat: add redux-logger

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,7 +1,11 @@
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import logger from 'redux-logger'
 import reducers from './reducer'
 
-const store = createStore(reducers, composeWithDevTools())
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(logger)),
+)
 
 export default store
