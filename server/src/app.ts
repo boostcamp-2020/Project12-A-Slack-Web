299f7501debc03d04179adc716470c89d84f48d1
hyex <hyexkim@gmail.com>
2020-12-03 03:47:10 +0900

    feat: add.socket to serverside

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,6 +14,7 @@ import { statusCode, resMessage } from '@util/constant'
 
 dotenv.config()
 
+import './socket'
 import passportConfig from '@util/passport-config'
 
 import apiRouter from '@controller/index'
