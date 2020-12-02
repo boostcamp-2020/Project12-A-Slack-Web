3bae4dbfbee3da2ebf7e337cfbff8505b2b4bd19
2oneweek <dlgkswn885@korea.ac.kr>
2020-12-02 17:40:43 +0900

    chore: change file name

--- client/src/page/Workspace/WorkspacJoinPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import M from '@molecule'
-
-const WorkspaceJoinPage = () => {
-  return <M.ButtonDiv>워크 스페이스 join 페이지</M.ButtonDiv>
-}
-
-export default WorkspaceJoinPage
