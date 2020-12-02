c97223e363ec3f9598700999987d5bec1efcf1b2
hyex <hyexkim@gmail.com>
2020-12-03 04:13:22 +0900

    feat: add socket to client-side

--- client/src/page/Channel/ChannelPage.tsx
+++ client/src/page/Channel/ChannelPage.tsx
@@ -5,6 +5,7 @@ import O from '@organism'
 import styled from 'styled-components'
 import { RootState } from '@store'
 import { getThreadsAsync } from '@store/reducer/thread.reducer'
+import { useSocket } from '../../socket'
 
 import channelInfo from './data'
 
@@ -17,6 +18,8 @@ const ChannelPage = () => {
   useEffect(() => {
     dispatch(getThreadsAsync.request(1))
   }, [])
+
+  useSocket()
   const channel = channelInfo
 
   const [subViewShow, setSubViewShow] = useState(false)
---
+++ client/src/socket.ts
@@ -0,0 +1,39 @@
+import { useEffect } from 'react'
+import { dispatch } from 'redux'
+import { io } from 'socket.io-client'
+import { receiveCreateThread } from '@store/reducer/thread.reducer'
+
+// const baseURL =
+//   process.env.NODE_ENV === 'development'
+//     ? process.env.SOCKET_SERVER_DOMAIN_DEVELOP
+//     : process.env.SOCKET_SERVER_DOMAIN_PRODUCTION
+// const socket = io(`${baseURL}/socket/1`)
+
+const socket = io(`http://127.0.0.1:4000/socket/1`)
+
+export const useSocket = () => {
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('connected')
+    })
+
+    socket.on('disconnect', () => {
+      console.log('disconnected')
+    })
+
+    socket.on('new message', (data: any) => {
+      console.log('new message: ', data)
+    })
+
+    // data: new thread
+    socket.on('RECEIVE_CREATE_THREAD', (data: any) => {
+      // 여기서 이벤트 호출
+      dispatch(receiveCreateThread(data))
+      console.log('new thread: ', data)
+    })
+
+    socket.emit('new message', 'username')
+  }, [])
+}
+
+export default socket
