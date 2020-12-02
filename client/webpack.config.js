e740352b27480dd25b867dc15e1050881df9202e
2oneweek <dlgkswn885@korea.ac.kr>
2020-12-02 18:48:52 +0900

    chore: add hoc alias

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
       '@store': path.resolve(__dirname, 'src/store'),
       '@util': path.resolve(__dirname, 'src/util'),
       '@api': path.resolve(__dirname, 'src/api'),
-      '@hook': path.resolve(__dirname, 'src/hook'),
+      '@hoc': path.resolve(__dirname, 'src/hoc'),
     },
   },
 
