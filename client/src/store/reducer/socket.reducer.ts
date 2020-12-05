ad73ecac93cc8676805b571474d05af1962904a7
hyex <hyexkim@gmail.com>
2020-12-06 04:20:47 +0900

    refactor: remove overlapping functions

--- client/src/store/reducer/socket.reducer.ts
+++ client/src/store/reducer/socket.reducer.ts
@@ -14,24 +14,18 @@ const initialState: SocketState = {
   socket: null,
 }
 
-const CONNECT_SOCKET = 'socket/CONNECT_SOCKET' as const
+export const CONNECT_SOCKET_REQUEST = 'socket/CONNECT_SOCKET_REQUEST' as const
 const CONNECT_SOCKET_SUCCESS = 'socket/CONNECT_SOCKET_SUCCESS' as const
 const CONNECT_SOCKET_ERROR = 'socket/CONNECT_SOCKET_ERROR' as const
 
-const connectSocket = createAction(CONNECT_SOCKET)()
-const connectSocketSuccess = createAction(CONNECT_SOCKET_SUCCESS)<Socket>()
-const connectSocketError = createAction(CONNECT_SOCKET_ERROR)<Error>()
-
-export const connectSocketAsync = createAsyncAction(
-  CONNECT_SOCKET,
+export const connectSocket = createAsyncAction(
+  CONNECT_SOCKET_REQUEST,
   CONNECT_SOCKET_SUCCESS,
   CONNECT_SOCKET_ERROR,
 )<undefined, Socket, Error>()
 
 const actions = {
-  connectSocket,
-  connectSocketSuccess,
-  connectSocketError,
+  ...connectSocket,
 }
 
 type SocketAction = ActionType<typeof actions>
