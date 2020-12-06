dce4de2340dbb06550814d52c45d2f94bb1767e2
hyex <hyexkim@gmail.com>
2020-12-07 01:16:33 +0900

    feat: enroll socket store and add actions
    - socket store 등록
    - create thread / join room 액션 추가

--- client/src/store/reducer/socket.reducer.ts
+++ client/src/store/reducer/socket.reducer.ts
@@ -14,24 +14,35 @@ const initialState: SocketState = {
   socket: null,
 }
 
-export const CONNECT_SOCKET_REQUEST = 'socket/CONNECT_SOCKET_REQUEST' as const
+const CONNECT_SOCKET_REQUEST = 'socket/CONNECT_SOCKET_REQUEST' as const
 const CONNECT_SOCKET_SUCCESS = 'socket/CONNECT_SOCKET_SUCCESS' as const
 const CONNECT_SOCKET_ERROR = 'socket/CONNECT_SOCKET_ERROR' as const
+const SEND_SOCKET_CREATE_THREAD = 'socket/SECT_SOCKET_CREATE_THREAD' as const
+const SEND_SOCKET_JOIN_ROOM = 'socket/SEND_SOCKET_JOIN_ROOM' as const
 
 export const connectSocket = createAsyncAction(
   CONNECT_SOCKET_REQUEST,
   CONNECT_SOCKET_SUCCESS,
   CONNECT_SOCKET_ERROR,
 )<undefined, Socket, Error>()
+export const sendSocketCreateThread = createAction(SEND_SOCKET_CREATE_THREAD)<{
+  channelId: number
+  threadId: number
+}>()
+export const sendSocketJoinRoom = createAction(SEND_SOCKET_JOIN_ROOM)<{
+  channelIdList: number[]
+}>()
 
 const actions = {
   ...connectSocket,
+  sendSocketCreateThread,
+  sendSocketJoinRoom,
 }
 
 type SocketAction = ActionType<typeof actions>
 
 const reducer = createReducer<SocketState, SocketAction>(initialState, {
-  [CONNECT_SOCKET]: (state, _) => state,
+  [CONNECT_SOCKET_REQUEST]: (state, _) => state,
   [CONNECT_SOCKET_SUCCESS]: (state, action) => ({
     socket: action.payload,
   }),
