9056b3bdff2594e99f2477021315b3611e95c53d
hyex <hyexkim@gmail.com>
2020-12-09 00:01:38 +0900

    refactor: edit message type

--- client/src/component/organism/ThreadDetail/index.ts
+++ client/src/component/organism/ThreadDetail/index.ts
@@ -1,23 +1,8 @@
-import { MessageType } from '@type/thread.type'
+import { GetThreadResponseType } from '@type/thread.type'
 
 export { default } from './ThreadDetail'
 
 export interface ThreadDetailProps {
-  thread: ThreadType
+  thread: GetThreadResponseType
   onReplyButtonClick: () => void
-}
-
-interface UserType {
-  id: number
-  email: string
-  name: string
-  profileImageUrl: string
-}
-
-interface ThreadType {
-  id: number
-  createdAt: string
-  updatedAt: string
-  Messages: MessageType[]
-  User: UserType
 }
