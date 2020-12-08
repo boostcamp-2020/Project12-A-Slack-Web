1208cfaa6cfdaa84ea1e6d69acd540449d86feab
kimseoyoung <caribou503@gmail.com>
2020-12-09 03:03:12 +0900

    feat: add joinMembersToChannel to fe

--- client/src/api/channel.ts
+++ client/src/api/channel.ts
@@ -1,6 +1,10 @@
 import myAxios from '@util/myAxios'
 
-import { ChannelRequestType, JoinChannelRequestType } from '@type/channel.type'
+import {
+  ChannelRequestType,
+  JoinChannelRequestType,
+  JoinMembersToChannelRequestType,
+} from '@type/channel.type'
 
 // TODO: workspace 전체의 채널을 어떻게 읽을 것인가
 const getChannels = async ({ workspaceId }: ChannelRequestType) => {
@@ -19,6 +23,17 @@ const joinChannel = async ({ channel, userId }: JoinChannelRequestType) => {
   return response.data
 }
 
+const joinMembersToChannel = async ({
+  channelId,
+  userList,
+}: JoinMembersToChannelRequestType) => {
+  const response = await myAxios.post({
+    path: `/channel/${channelId}/join-members`,
+    data: { userList },
+  })
+  return response.data
+}
+
 const getChannelInfo = async (channelId: number) => {
   const response = await myAxios.get({
     path: `/channel/${channelId}`,
@@ -27,4 +42,9 @@ const getChannelInfo = async (channelId: number) => {
   return response.data
 }
 
-export default { getChannels, joinChannel, getChannelInfo }
+export default {
+  getChannels,
+  joinChannel,
+  getChannelInfo,
+  joinMembersToChannel,
+}
