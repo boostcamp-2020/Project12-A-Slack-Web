ced026d364ad702b5975163a35c813dfc98135e0
kimseoyoung <caribou503@gmail.com>
2020-12-10 17:08:46 +0900

    feat: add readReactionById to reaction service layer
    readReactionById 추가
    + createReaction 시 생성된 리액션 id 반환하도록 수정

--- server/src/service/reaction.service.ts
+++ server/src/service/reaction.service.ts
@@ -1,5 +1,7 @@
 import ReactionModel from '@model/reaction.model'
+import UserModel from '@model/user.model'
 import { statusCode, resMessage } from '@util/constant'
+import validator from '@util/validator'
 
 interface ReactionType {
   content?: string
@@ -33,10 +35,14 @@ const createReaction = async ({ userId, content, messageId }: ReactionType) => {
     }
   }
   try {
-    await ReactionModel.create({ userId, content, messageId })
+    const newReaction = await ReactionModel.create({
+      userId,
+      content,
+      messageId,
+    })
     return {
       code: statusCode.CREATED,
-      json: { success: true },
+      json: { success: true, data: { reactionId: newReaction.id } },
     }
   } catch (error) {
     console.log(error)
@@ -69,4 +75,35 @@ const deleteReaction = async ({ reactionId }: ReactionType) => {
   }
 }
 
-export default { createReaction, deleteReaction }
+const readReactionById = async ({ id }: { id: number }) => {
+  if (!validator.isNumber(id)) {
+    return {
+      code: statusCode.BAD_REQUEST,
+      json: { success: false, message: resMessage.OUT_OF_VALUE },
+    }
+  }
+  try {
+    const reaction = await ReactionModel.findOne({
+      attributes: ['id', 'content', 'messageId', 'createdAt', 'updatedAt'],
+      include: [
+        { model: UserModel, attributes: ['id', 'name', 'profileImageUrl'] },
+      ],
+      where: { id },
+    })
+    return {
+      code: statusCode.OK,
+      json: {
+        success: true,
+        data: reaction,
+      },
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      code: statusCode.DB_ERROR,
+      json: { success: false, message: resMessage.DB_ERROR },
+    }
+  }
+}
+
+export default { createReaction, deleteReaction, readReactionById }
