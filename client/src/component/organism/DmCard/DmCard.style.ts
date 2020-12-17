2925d3c8f6213be6d5877f55d200be0d6925c04a
zunopark <enzoribery@naver.com>
2020-12-18 01:16:31 +0900

    refactor: fix css all dm page

--- client/src/component/organism/DmCard/DmCard.style.ts
+++ client/src/component/organism/DmCard/DmCard.style.ts
@@ -1,20 +1,28 @@
 import styled from 'styled-components'
 
-const DmCardImageContainer = styled.div`
-  position: relative;
-  height: 3rem;
-  display: flex;
-`
-
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 20px;
+  width: 100%;
 `
 
 const DmCardContent = styled.div`
   display: flex;
+  justify-content: space-between;
   width: 100%;
+`
+
+const DmCardFirstSection = styled.div`
+  display: flex;
+`
+
+const DmCardSecondSection = styled.div``
+
+const DmCardImageContainer = styled.div`
+  position: relative;
+  height: 3rem;
+  display: flex;
 `
 
 const dmCardButtonStyle = {
@@ -52,4 +60,6 @@ export default {
   DmCardContent,
   Container,
   DmCardImageContainer,
+  DmCardSecondSection,
+  DmCardFirstSection,
 }
