7fb0e74549cbf90122b3cacee024694c5780a6aa
kimseoyoung <caribou503@gmail.com>
2020-12-17 20:02:43 +0900

    style: modify SendEmailModal css

--- client/src/component/molecule/SendEmailModal/SendEmailModal.style.ts
+++ client/src/component/molecule/SendEmailModal/SendEmailModal.style.ts
@@ -11,32 +11,40 @@ const InvitePeopleContainer = styled.div`
 
 const InvitePeopleTitle = styled.div`
   width: 100%;
-  height: 30%;
   display: flex;
   justify-content: center;
   align-items: center;
+  margin: 25px 0;
 `
 
+const InputFormWrapper = styled.div`
+  width: 100%;
+  height: 30%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 5px 0;
+`
 const InvitePeopleInput = styled.div`
   width: 100%;
-  height: 30%;
   display: flex;
   justify-content: center;
   align-items: center;
+  margin: 15px;
 `
 
-const InvitePeopleTextInput = styled.div`
+const InvitePeopleLink = styled.div`
   width: 100%;
-  height: 30%;
   display: flex;
-  justify-content: center;
-  align-items: center;
   flex-direction: column;
+  align-items: center;
+  margin: 10px 0;
 `
 
 export default {
-  InvitePeopleTextInput,
   InvitePeopleInput,
+  InputFormWrapper,
   InvitePeopleContainer,
   InvitePeopleTitle,
+  InvitePeopleLink,
 }
