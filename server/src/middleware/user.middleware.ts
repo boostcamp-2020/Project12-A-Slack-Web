fa9bcc9af8383fe34f115e9151d2751c0328797d
2oneweek <dlgkswn885@korea.ac.kr>
2020-12-21 02:20:59 +0900

    fix: auth hoc

--- server/src/middleware/user.middleware.ts
+++ server/src/middleware/user.middleware.ts
@@ -14,12 +14,22 @@ const verifyUser = (req: Request, res: Response, next: NextFunction) => {
     headers: { authorization },
   } = req
   if (!authorization) return res.status(403).json({ success: false })
-  const token = authorization.replace(/bearer /i, '')
-  const { id, email, name, profileImageUrl } = jwt.checkToken(token) as UserInfo
-  const isUser = checkUser({ id, email, name })
-  if (!isUser) return res.status(204).json({ success: false })
-  req.user = { id, email, name, profileImageUrl }
-  return next()
+
+  const token = authorization.replace(/Bearer /i, '')
+
+  try {
+    if (token) {
+      const { id, email, name, profileImageUrl } = jwt.checkToken(
+        token,
+      ) as UserInfo
+      const isUser = checkUser({ id, email, name })
+      if (!isUser) return res.status(204).json({ success: false })
+      req.user = { id, email, name, profileImageUrl }
+      return next()
+    }
+  } catch (error) {
+    return res.status(403).json({ success: false })
+  }
 }
 
 export default verifyUser
